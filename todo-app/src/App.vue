d87ca0e78684e05fb0a923f080b6bbd69bb0841f
Parsa2345 <jack.kamard@gmail.com>
2023-10-24 12:35:12 +0330

    up project

---
+++ todo-app/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <body>
+    <AppHeader></AppHeader>
+
+    <main>
+      <AddToDo></AddToDo>
+      <Todo></Todo>
+      <div class="card stat">
+        <p class="corner"><span id="items-left">0</span> مورد باقی مانده</p>
+        <div class="filter">
+          <button id="all" class="on">همه</button>
+          <button id="active">فعال</button>
+          <button id="completed">تکمیل</button>
+        </div>
+        <div class="corner">
+          <button id="clear-completed">حذف تکمیل شده ها</button>
+        </div>
+      </div>
+    </main>
+    <AppFooter></AppFooter>
+  </body>
+  <!-- 
+  <AppHeader></AppHeader>
+  <AddToDo></AddToDo>
+  <Todo></Todo>
+  
+  <div class="card stat">
+      <p class="corner"><span id="items-left">0</span> مورد باقی مانده</p>
+      <div class="filter">
+        <button id="all" class="on">همه</button>
+        <button id="active" >فعال</button>
+        <button id="completed">تکمیل</button>
+      </div>
+      <div class="corner">
+        <button id="clear-completed">حذف تکمیل شده ها</button>
+      </div>
+    </div>
+  
+  <AppFooter></AppFooter>  -->
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AddToDo from './components/AddToDo.vue';
+import Todo from './components/Todo.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AddToDo,
+    Todo,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo-app/src/components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+    <p> برای مرتب کردن وظایف میتوانید وظیفه موردنظر را بکشید و رها کنید - Drag And Drop</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+
+</script>
